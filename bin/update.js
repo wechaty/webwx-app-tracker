6e0d73eaa5c5f40dd879448f9bdc7a2c2a17f3af
Zhuohuan LI (CARPE DIEM) <zixia@zixia.net>
2016-11-09 04:27:20 +0000

    auto updater

---
+++ bin/update.js
@@ -0,0 +1,60 @@
+/**
+ * webwxApp SPA Html &amp; AngularJS Script Tracker for Web Wechat
+ *  https://github.com/wechaty/webwx-app/
+ *
+ */
+const { execSync }      = require('child_process')
+const { writeFileSync } = require('fs')
+
+function get(url) {
+  return execSync('curl -s ' + url)
+          .toString()
+}
+
+function getJsUrl(html) {
+  //  <script type="text/javascript" src="https://res.wx.qq.com/zh_CN/htmledition/v2/js/webwxApp31aa32.js"></script>
+  const re = new RegExp(' src="(https://res.wx.qq.com/.+?/js/webwxApp.+?\.js)"></script>', 'i')
+  const match = re.exec(html)
+  if (!match) {
+    return null
+  }
+  return match[1]
+}
+
+function getJsVer(url) {
+  const re = /webwxApp(.+?)\.js/i
+  const match = re.exec(url)
+  if (!match) {
+    throw new Error('no version')
+  }
+  return match[1]
+}
+
+function htmlBeautify(file) {
+  return execSync('html-beautify ' + file)
+          .toString()
+}
+
+function jsBeautify(file) {
+  return execSync('js-beautify ' + file)
+          .toString()
+}
+
+const html = get('https://wx.qq.com')
+const jsUrl = getJsUrl(html)
+const jsVer = getJsVer(jsUrl)
+const js = get(jsUrl)
+
+const orignalHtmlFile = 'orignal/index.html'
+const orignalJsFile   = 'orignal/webwxApp.js'
+
+const formattedHtmlFile = 'formatted/index.html'
+const formattedJsFile   = 'formatted/webwxApp.js'
+
+writeFileSync(orignalHtmlFile , html)
+writeFileSync(orignalJsFile   , js)
+
+writeFileSync(formattedHtmlFile , htmlBeautify(orignalHtmlFile))
+writeFileSync(formattedJsFile   , jsBeautify(orignalJsFile))
+
+console.log(jsVer)
