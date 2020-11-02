c1a800b6af1d5faf9ab2eac156bd2f5eaefc3fcc
Huan LI (李卓桓) <zixia@zixia.net>
2020-11-02 18:55:40 +0800

    add github actions crontab

--- README.md
+++ README.md
@@ -1,53 +1,76 @@
 # webwx-app-tracker
+
 webwxApp SPA Html &amp; AngularJS Script Tracker for Web Wechat
 
-# Introduction
+## Introduction
 
 The purpose of creating this repository is to track the code change from https://wx.qq.com , which can make the [PuppetWeb of Wechaty](https://github.com/wechaty/wechaty-puppet-puppeteer) life easier in the future.
 
 What it is doing is:
 
-1. Save https://wx.qq.com HTML to `orignal/index.html`
-1. Save AngularJs JavsScript to `orignal/wxwebApp.js`
+1. Save <https://wx.qq.com> HTML to `original/index.html`
+1. Save AngularJs JavaScript to `original/wxwebApp.js`
 1. Format the above two files, and save them to `formatted/`
 1. Check wether there has any update
 1. Commit to Git Repository and use js file name as commit message(if updated)
 
-# Case Study
+## Case Study
 
 I have a webwxApp.js which was saved on May 2016, about 5 months ago before I setup this tracker.
 
 1. After I finished this tracker, I got a diff on webwxApp between version May to Nov, you can see, there's no much change at the past half year:
 
-  https://github.com/wechaty/webwx-app-tracker/commit/ab292418e2651ec7e2630da156b3c18f0dca26fd
+  <https://github.com/wechaty/webwx-app-tracker/commit/ab292418e2651ec7e2630da156b3c18f0dca26fd>
 
-2. However there was a new little bug introduced in, it's a typo of `API_webwxsendmsgvedio`:
+1. However there was a new little bug introduced in, it's a typo of `API_webwxsendmsgvedio`:
 
-  https://github.com/wechaty/webwx-app-tracker/commit/ab292418e2651ec7e2630da156b3c18f0dca26fd#diff-80c9da80c8ca080b37c6c49864880646R3300
+  <https://github.com/wechaty/webwx-app-tracker/commit/ab292418e2651ec7e2630da156b3c18f0dca26fd#diff-80c9da80c8ca080b37c6c49864880646R3300>
 
 Enjoy it!
 
-# Quick Start
+## Quick Start
 
 Open https://github.com/wechaty/webwx-app-tracker/commits/master/formatted/webwxApp.js to know the day to day change of web wechat app code.
 
 Or clone this repository on github, then run on your own machine.
 
-# Cron
+## Cron
 
 I put `npm run track` to my crontab with the following setting:(run every hour)
 
 ```shell
-zixia@dev:~/git/webwx-app-tracker$ crontab -l
+huan@dev:~/git/webwx-app-tracker$ crontab -l
 1 * * * * (date && cd ~/git/webwx-app-tracker && npm run -s track >> /tmp/webwx-app-tracker.log 2>&1)
 ```
 
 P.S. You need to use Github Personal Token to enable push in crontab:
 
-* http://stackoverflow.com/questions/18935539/authenticate-with-github-using-token
+* <http://stackoverflow.com/questions/18935539/authenticate-with-github-using-token>
 
 Any suggestion is welcome.
 
-# See Also
+## See Also
 
 * [网页QQ/微信/新浪微博等重要文件监控，方便发现文件变动，及时更新相关项目](https://github.com/sjdy521/Mojo-URLMonitor)
+
+## History
+
+### v0.2 Nov 2020
+
+Use GitHub Actions to replace crontab
+
+### v0.1 Nov 2016
+
+Use crontab to run `npm run track`
+
+## Author
+
+[Huan](https://github.com/huan) [(李卓桓)](http://linkedin.com/in/zixia), Tencent TVP of Chatbot
+
+[![Profile of Huan LI (李卓桓) on StackOverflow](https://stackoverflow.com/users/flair/1123955.png)](https://stackoverflow.com/users/1123955/huan)
+
+## Copyright & License
+
+* Code & Docs © 2016-now Wechaty Community Contributors
+* Code released under the Apache-2.0 License
+* Docs released under Creative Commons
--- bin/track.js
+++ bin/track.js
@@ -10,11 +10,11 @@ const { writeFileSync } = require('fs')
 const { format }        = require('util')
 
 const VERSION_HISTORY = 'history-version.json'
-const versionHistory      = require('../' + VERSION_HISTORY)
+const versionHistory  = require('../' + VERSION_HISTORY)
 
 function get(url) {
   return execSync('curl -s ' + url)
-  .toString()
+    .toString()
 }
 
 function getJsUrl(html) {
@@ -40,18 +40,18 @@ function getJsVer(url) {
 
 function htmlBeautify(file) {
   return execSync('html-beautify ' + file)
-  .toString()
+    .toString()
 }
 
 function jsBeautify(file) {
   return execSync('js-beautify ' + file)
-  .toString()
+    .toString()
 }
 
 function gitDiff() {
   const n = execSync('git diff --name-only | wc -l')
-  .toString()
-  .replace(/\n/, '')
+    .toString()
+    .replace(/\n/, '')
   return n > 0
 }
 
@@ -60,11 +60,11 @@ function gitCommit(message) {
 }
 
 function gitPull() {
-  execSync('git pull >/dev/null 2>&1') // hide token for output
+  execSync('git pull') // hide token for output
 }
 
 function gitPush() {
-  execSync('git push >/dev/null 2>&1') // hide token for output
+  execSync('git push') // hide token for output
 }
 
 function log(message) {
@@ -85,7 +85,7 @@ function log(message) {
   if (month < 10) { month = '0' + month }
 
   const desc =  format('%s-%s-%s %s:%s:%s '
-                        , now.getFullYear() 
+                        , now.getFullYear()
                         , (now.getMonth() + 1)
                         , date
                         , hour
@@ -96,70 +96,73 @@ function log(message) {
   console.log(desc + format.apply(null, arguments))
 }
 
-gitPull()
+function main () {
+  gitPull()
 
-const html = get('https://wx.qq.com')
-const jsUrl = getJsUrl(html)
-const jsVer = getJsVer(jsUrl)
-const js = get(jsUrl)
+  const html = get('https://wx.qq.com')
+  const jsUrl = getJsUrl(html)
+  const jsVer = getJsVer(jsUrl)
+  const js = get(jsUrl)
 
-const orignalHtmlFile = 'orignal/index.html'
-const orignalJsFile   = 'orignal/webwxApp.js'
+  const originalHtmlFile = 'original/index.html'
+  const originalJsFile   = 'original/webwxApp.js'
 
-const formattedHtmlFile = 'formatted/index.html'
-const formattedJsFile   = 'formatted/webwxApp.js'
+  const formattedHtmlFile = 'formatted/index.html'
+  const formattedJsFile   = 'formatted/webwxApp.js'
 
-writeFileSync(orignalHtmlFile , html)
-writeFileSync(orignalJsFile   , js)
+  writeFileSync(originalHtmlFile , html)
+  writeFileSync(originalJsFile   , js)
 
-writeFileSync(formattedHtmlFile , htmlBeautify(orignalHtmlFile))
-writeFileSync(formattedJsFile   , jsBeautify(orignalJsFile))
+  writeFileSync(formattedHtmlFile , htmlBeautify(originalHtmlFile))
+  writeFileSync(formattedJsFile   , jsBeautify(originalJsFile))
 
-if (!gitDiff()) {
-  log('nothing new; current: %s', jsVer)
-  return
-}
-
-/**
- * check jsVer
- */
-if (!jsVer) {
-  log('jsVer empty')
-  return
-}
-
-if (jsVer in versionHistory) {
-  const latestVer = versionHistory['latest']
-
-  if (latestVer === jsVer) {
-    log('jsVer is the latest, but local has gitDiff()')
-  } else {
-    const latestVerDate = versionHistory[latestVer]
-    jsVerDate = versionHistory[jsVer]
-
-    log('jsVer found %s[%s] latest %s[%s]. maybe Fifty Shades of Grey?'
-        , jsVer, jsVerDate
-        , latestVer, latestVerDate
-    )
+  if (!gitDiff()) {
+    log('nothing new; current: %s', jsVer)
     return
   }
-} else {
 
-   /**
-   * Save new version to history json file
+  /**
+   * check jsVer
    */
-  versionHistory[jsVer] = new Date()
-  versionHistory['latest'] = jsVer
-  
-  const json = JSON.stringify(versionHistory, null, '  ')
-  writeFileSync(VERSION_HISTORY, json)
+  if (!jsVer) {
+    log('jsVer empty')
+    return
+  }
+
+  if (jsVer in versionHistory) {
+    const latestVer = versionHistory['latest']
+
+    if (latestVer === jsVer) {
+      log('jsVer is the latest, but local has gitDiff()')
+    } else {
+      const latestVerDate = versionHistory[latestVer]
+      jsVerDate = versionHistory[jsVer]
+
+      log('jsVer found %s[%s] latest %s[%s]. maybe Fifty Shades of Grey?'
+          , jsVer, jsVerDate
+          , latestVer, latestVerDate
+      )
+      return
+    }
+  } else {
+
+    /**
+     * Save new version to history json file
+     */
+    versionHistory[jsVer] = new Date()
+    versionHistory['latest'] = jsVer
+
+    const json = JSON.stringify(versionHistory, null, '  ')
+    writeFileSync(VERSION_HISTORY, json)
+  }
+
+  /**
+   * Commit & Push
+   */
+  gitCommit('webwx-' + jsVer)
+  gitPush()
+
+  log('new version: %s', jsVer)
 }
 
-/**
- * Commit & Push
- */
-gitCommit('webwx-' + jsVer)
-gitPush()
-
-log('new version: %s', jsVer)
-
+main()
